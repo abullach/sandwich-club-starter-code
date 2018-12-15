30f71b59b7aa82205c69cb760e624569ada1d9f2
Antonius Bullach <abullach@yahoo-inc.com>
2018-12-15 15:44:22 -0800

    - Added utility functions to handle Sandwich JSON data. - Updated DetailActivity to dynamically populate the UI depending on available sandwich data - Added layout for the sandwich details using ConstraintLayout - Added up navigation on DetailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,8 +2,14 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -14,20 +20,42 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private static final int DEFAULT_MARGIN = 8;
+    private ImageView mSandwichIv;
+    private TextView mName;
+    private TextView mOrigin;
+    private TextView mPlaceOfOriginLabel;
+    private TextView mAlsoKnownAsLabel;
+    private TextView mAlsoKnown;
+    private TextView mDescription;
+    private TextView mIngredients;
+    private Sandwich mSandwich;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        if(getSupportActionBar() != null){
+            // Set up navigation in action bar
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        mSandwichIv = findViewById(R.id.image_iv);
+        mName = findViewById(R.id.name_tv);
+        mOrigin = findViewById(R.id.origin_tv);
+        mPlaceOfOriginLabel = findViewById(R.id.place_of_origin_label_tv);
+        mAlsoKnownAsLabel = findViewById(R.id.also_known_as_label_tv);
+        mAlsoKnown = findViewById(R.id.also_known_tv);
+        mDescription = findViewById(R.id.description_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        int position = intent != null ? intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION) : 0;
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -36,19 +64,13 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
+        mSandwich = JsonUtils.parseSandwichJson(json);
+        if (mSandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
-
         populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +78,89 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Dynamically populate the UI on the DetailActivity depending on available sandwich data.
+     */
     private void populateUI() {
+        // Set action bar title
+        setTitle(mSandwich.getMainName());
 
+        Picasso.with(this)
+                .load(mSandwich.getImage())
+                .error(R.mipmap.ic_launcher) // Display fallback image if image cannot be loaded
+                .into(mSandwichIv);
+        // Set action bar title
+        setTitle(mSandwich.getMainName());
+        // Set sandwich name
+        mName.setText(mSandwich.getMainName());
+
+        ConstraintSet set = new ConstraintSet();
+        ConstraintLayout layout;
+        layout = findViewById(R.id.constraintlayout);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        // Set 'also known as' name if exist and make visible
+        if(mSandwich.getAlsoKnownAs() != null) {
+            for (String s: mSandwich.getAlsoKnownAs()) {
+                stringBuilder.append(s);
+                stringBuilder.append(", ");
+            }
+            mAlsoKnown.setVisibility(View.VISIBLE);
+            mAlsoKnownAsLabel.setVisibility(View.VISIBLE);
+            mAlsoKnown.setText(stringBuilder.toString());
+        } else {
+            set.clone(layout);
+            // Break the connection
+            set.clear(R.id.origin_tv, ConstraintSet.TOP);
+            set.clear(R.id.place_of_origin_label_tv, ConstraintSet.TOP);
+            // Make a new connection with name TextView and set constraints
+            set.connect(R.id.origin_tv, ConstraintSet.TOP, R.id.name_tv, ConstraintSet.BOTTOM, DEFAULT_MARGIN);
+            set.connect(R.id.place_of_origin_label_tv, ConstraintSet.TOP, R.id.name_tv, ConstraintSet.BOTTOM, DEFAULT_MARGIN);
+            set.applyTo(layout);
+        }
+
+        // Set 'place of origin' name if exist and make visible
+        if(mSandwich.getPlaceOfOrigin() != null && !mSandwich.getPlaceOfOrigin().equals("")) {
+            mPlaceOfOriginLabel.setVisibility(View.VISIBLE);
+            mOrigin.setVisibility(View.VISIBLE);
+            mOrigin.setText(mSandwich.getPlaceOfOrigin());
+        } else {
+            set.clone(layout);
+            // Break the connection
+            set.clear(R.id.description_tv, ConstraintSet.TOP);
+            set.clear(R.id.description_label_tv, ConstraintSet.TOP);
+            // Set a new connection in layout depending if also known as name is available
+            if(mSandwich.getAlsoKnownAs() != null) {
+                // Make a new connection with also known TextView and set constraints
+                set.connect(R.id.description_tv, ConstraintSet.TOP, R.id.also_known_tv, ConstraintSet.BOTTOM, DEFAULT_MARGIN);
+                set.connect(R.id.description_label_tv, ConstraintSet.TOP, R.id.also_known_as_label_tv, ConstraintSet.BOTTOM, DEFAULT_MARGIN);
+            } else {
+                // Make a new connection with name TextView and set constraints
+                set.connect(R.id.description_tv, ConstraintSet.TOP, R.id.name_tv, ConstraintSet.BOTTOM, DEFAULT_MARGIN);
+                set.connect(R.id.description_label_tv, ConstraintSet.TOP, R.id.name_tv, ConstraintSet.BOTTOM, DEFAULT_MARGIN);
+            }
+            set.applyTo(layout);
+        }
+        // Set sandwich description text
+        mDescription.setText((mSandwich.getDescription()));
+        StringBuilder builder = new StringBuilder();
+        // Set list of ingredients
+        for (String s: mSandwich.getIngredients()) {
+            builder.append("* ");
+            builder.append(s);
+            builder.append("\n");
+        }
+        mIngredients.setText(builder.toString());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,79 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility functions to handle Sandwich JSON data.
+ */
 public class JsonUtils {
 
+    /**
+     * This method parses JSON representing a single sandwich and converts it into Sandwich model object.
+     * <br>
+     * @param json String with JSON from a single sandwich object
+     * @return Sandwich model object
+     */
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        final String SANDWICH_NAME_OBJECT = "name";
+
+        /* All names are children of the "name' object */
+        final String SANDWICH_NAME_MAIN = "mainName";
+        final String SANDWICH_NAME_KNOWN_AS = "alsoKnownAs";
+
+        final String SANDWICH_ORIGIN = "placeOfOrigin";
+        final String SANDWICH_DESCRIPTION = "description";
+        final String SANDWICH_IMAGE = "image";
+        final String SANDWICH_INGREDIENTS = "ingredients";
+
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            /* Get the JSON object representing the sandwich name */
+            JSONObject nameObject = sandwichJson.getJSONObject(SANDWICH_NAME_OBJECT);
+            /* Set sandwich object field "mainName" from the JSON representing the sandwich main name */
+            sandwich.setMainName(nameObject.getString(SANDWICH_NAME_MAIN));
+            // Get the alternative name(s) from the JSONArray representing the sandwich alsoKnownAs array
+            JSONArray knownAsArray = nameObject.getJSONArray(SANDWICH_NAME_KNOWN_AS);
+            if(knownAsArray.length() != 0) {
+                List<String> knownAsList = new ArrayList<>(knownAsArray.length());
+                for(int i = 0; i < knownAsArray.length(); i++){
+                    // Add name to the list of alternative names
+                    knownAsList.add(knownAsArray.getString(i));
+                }
+                /* Set sandwich object field "alsoKnownAs" from the list of alternative name(s) */
+                sandwich.setAlsoKnownAs(knownAsList);
+            }
+            /* Set sandwich object field "placeOfOrigin" from the JSON representing the sandwich place of origin */
+            sandwich.setPlaceOfOrigin(sandwichJson.optString(SANDWICH_ORIGIN));
+            /* Set sandwich object field "description" from the JSON representing the sandwich description */
+            sandwich.setDescription(sandwichJson.optString(SANDWICH_DESCRIPTION));
+            /* Set the image url to sandwich object field "image" from the JSON representing the sandwich image */
+            sandwich.setImage(sandwichJson.optString(SANDWICH_IMAGE));
+            // Get the ingredient(s) from the JSONArray representing the sandwich ingredients
+            JSONArray ingredientsArray = sandwichJson.getJSONArray(SANDWICH_INGREDIENTS);
+            if(ingredientsArray.length() !=0) {
+                List<String> ingredients = new ArrayList<>(ingredientsArray.length());
+                for(int x = 0; x < ingredientsArray.length(); x++){
+                    // Add ingredient to the list of ingredients
+                    ingredients.add(ingredientsArray.getString(x));
+                }
+                /* Set sandwich object field "ingredients" from the list of ingredients */
+                sandwich.setIngredients(ingredients);
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        // Return new sandwich object
+        return sandwich;
     }
 }
